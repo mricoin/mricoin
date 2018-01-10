f93042dacc18014cf82261207c4b813067c0e5c4
mricoin <info@teamjx.de>
2018-01-10 04:54:09 +0100

    changes

--- mricoin/README.md
+++ mricoin/README.md
@@ -1,3 +1,3 @@
-FooCoin (FOO)
+MRICoin (MRI)
 
 Blah, blah, blah.
--- mricoin/src/main.cpp
+++ mricoin/src/main.cpp
@@ -2014,13 +2014,13 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1369623856; //epochtime
+        block.nTime    = 1515556128; //epochtime
         block.nBits    = 0x1e0ffff0;
         block.nNonce   = 1345972;
 
         if (fTestNet)
         {
-            block.nTime    = 1369591342;
+            block.nTime    = 1515556128;
             block.nNonce   = 440824;
         }
 
